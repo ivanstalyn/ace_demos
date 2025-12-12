032412730b11fb6fdeb91dcfbca0098bd19c2ff8
Ivan Stalyn Muela Flor <imuela@ec.ibm.com>
2025-12-11 22:58:52 -0500

    se eliminan variables de configuración. Se compila el código en la imagen directamente

--- container/shell/ace_manage.sh
+++ container/shell/ace_manage.sh
@@ -17,35 +17,11 @@ start()
     echo "************************************************************************"
     ${MQSI_BASE_FILEPATH}/ace version
     echo "************************************************************************"
-
-    if [ ! -f "/tmp/temp.bar" ]; 
-    then
-        echo " ";
-        echo "************************************************************************"
-        echo "Desplegando aplicación en servidor de integración ${ACE_SERVER_NAME} ..."
-        echo "************************************************************************"
-        
-        ibmint package --compile-maps-and-schemas --input-path proyectos --output-bar-file /tmp/temp.bar  2>&1 | tee -a /tmp/deploys
-        ibmint apply overrides proyectos/overrides/* --bar-file /tmp/temp.bar 2>&1 | tee -a /tmp/deploys
-        ibmint deploy --input-bar-file /tmp/temp.bar --output-work-directory ${ACE_WORKPATH}/ 2>&1 | tee -a /tmp/deploys
-        echo " ";
-        echo "************************************************************************"
-        echo "Optimizando servidor de integración ${ACE_SERVER_NAME} ..."
-        echo "************************************************************************"
-        ibmint optimize server --work-directory ${ACE_WORKPATH}
-    else
-        echo " ";
-        echo "************************************************************************"
-        echo "Aplicación ya está desplegada en servidor de integración ${ACE_SERVER_NAME}."
-        echo "Servidor de integración ${ACE_SERVER_NAME} ya está optimizado."
-        echo "************************************************************************"
-    fi
-
     echo " ";
     echo "************************************************************************"
     echo "Inicializando servidor de integración ${ACE_SERVER_NAME} ..."
     echo "************************************************************************"
-    IntegrationServer --work-dir ${ACE_WORKPATH} --name ${ACE_SERVER_NAME}
+    IntegrationServer --work-dir /home/aceuser/ace-server --name ${ACE_SERVER_NAME}
 
 }
 
